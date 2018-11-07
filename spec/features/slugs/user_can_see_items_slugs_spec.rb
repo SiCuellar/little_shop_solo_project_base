f28f40f48d8a8f4eb61ccf95c080269ac97d1371
SilvestreCuellar <cuellarsilvestre@gmail.com>
2018-11-06 21:46:47 -0700

    passing basic slug test, fixing breaking test

---
+++ spec/features/slugs/user_can_see_items_slugs_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe "user sees one item" do
+  it "on item show page with item attributes" do
+    @merchant = create(:merchant)
+
+    item_1 = create(:item, user: @merchant)
+    item_2 = create(:item, user: @merchant)
+
+    visit item_path(item_1)
+
+    expect(current_path).to eq(item_path(item_1))
+
+    expect(page).to have_content(item_1.name)
+    expect(page).to have_content(item_1.price)
+    expect(page).to have_content(item_1.description)
+
+    expect(page).to_not have_content(item_2.name)
+    expect(page).to_not have_content(item_2.price)
+    expect(page).to_not have_content(item_2.description)
+  end
+end
